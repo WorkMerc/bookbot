b1d57c832f5e29fd97fc1f98449bbdaf44b6095e
theworkmerc <theworkmerc@gmail.com>
2025-12-24 14:32:50 -0700

    updated code for initial functions.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-def get_book_text():
-    #print("Hello! And welcome to bookbot, the automated system for counting the words and letters in a book!")
-    #print("Please enter the relative file path to the book you would like to parse:")
-    book_title = "./books/frankenstein.txt"
-    with open(book_title) as book:
-        return print(book.read())
+def get_book_text(path):
+    with open(path) as book:
+        return book.read()
     
-get_book_text()
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+main()
