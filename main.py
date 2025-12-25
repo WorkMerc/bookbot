476e1d887809ab1003bd92e6d47bee14da9ddeb3
theworkmerc <theworkmerc@gmail.com>
2025-12-24 20:18:20 -0700

    Modified stats.py and main.py with new functions: word_split for breaking text into list, word_count for counting the length of the word_split list, char_count creates a dictionary of characters, makes them all lower case and counts how many occur in the text.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count
+from stats import word_split, word_count, char_count
  
 #This function opens the .txt file that is needed for the count function
 def get_book_text(path):
@@ -9,7 +9,10 @@ def get_book_text(path):
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    num_words = word_count(text)
+    word_list = word_split(text)
+    num_words = word_count(word_list)
+    num_chars = char_count(word_list)
     print(f'Found {num_words} total words')
+    print(f'Found {num_chars} in total of each character')
 
 main()
--- stats.py
+++ stats.py
@@ -1,5 +1,18 @@
 #This function splits the words from get_book_text into a list and counts the words that were returned
-def word_count(text):
-    words = text.split()
-    total_words = len(words)
+def word_split(text):
+    word_list = text.split()
+    return word_list
+
+def word_count(word_list):
+    total_words = len(word_list)
     return total_words
+
+def char_count(text):
+    characters = {}
+    string_text = str(text)
+    for character in string_text.lower():
+        if character not in characters:
+            characters[character] = 1
+        else:
+            characters[character] += 1
+    return characters
